91bdf057a230f7c3caf7dafe2a450aa3e735f3a3
CodeMeKathy * <Kathy@KathyLambert.me>
2020-06-20 12:37:25 -0400

    Remove unneeded directories

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-import Hero from './Components/Hero/Hero'
+import Hero from './Components/Hero'
 
 function App() {
   return (
