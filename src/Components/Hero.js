4184af900b343865aacb6127a44f5bd4a4c83832
CodeMeKathy * <Kathy@KathyLambert.me>
2020-06-20 12:44:00 -0400

    Add starter functions to Hero Component

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
 
+
+// Old React class component setup
 export class Hero extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-      </div>
+      
     )
   }
+}
+
+// New React => Functional Component
+const Hero = props => {
+  return(
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
 }
 
 export default Hero
