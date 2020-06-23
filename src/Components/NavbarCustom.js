ca15b62c8fc7af302e34fcae985785028fd0eede
vai1205 <vaibhav.12may95@gmail.com>
2020-06-23 22:46:07 +0530

    Complete Navbar and Make it mobile responsive
    Co-Authored-By: Kathy Lambert <codemekathy@users.noreply.github.com>

--- src/Components/NavbarCustom.js
+++ src/Components/NavbarCustom.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 // React Bootstrap Imports
 import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 // Custom Imports
 import logo from '../logo.svg'
@@ -9,17 +10,22 @@ import logo from '../logo.svg'
 const NavbarCustom = props => {
   return (
     <>
-      <Navbar>
-        <Navbar.Brand href='#home'>
-          <img
-            alt=''
-            src={logo}
-            width='30'
-            height='30'
-            className='d-inline-block align-top'
-          />
-          Spotify Dash
-        </Navbar.Brand>
+      <Navbar expand="lg">
+        <Navbar.Brand href="#home"><img
+          alt=''
+          src={logo}
+          width='30'
+          height='30'
+          className='d-inline-block align-top'
+        />Spotify Dash</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav style={{ placeItems: "flex-end" }}>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#link">Contact Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </>
   )
